9f5cb86d825cdb94180cd84490102c1e0e6f12df
naveen <naveenchowdaryn1209@gmail.com>
2020-02-06 20:31:09 +0530

    commited successfully

---
+++ javex.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class gift
+{
+int sw,cw,bw,num;
+public void weight(int s,int c)
+{
+sw=s*5;
+cw=c*2;
+bw=sw+cw;
+num=c+s;
+}
+}
+class chocolates extends gift
+{
+public  void tw()
+{
+System.out.println("total weight of gift box="+bw+"gms");
+}
+}
+class sweets extends gift
+{
+public void num()
+{
+System.out.println("total no of candies in box="+num);
+}
+}
+class javex
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no of Sweets :");
+int n1=sc.nextInt();
+System.out.println("enter the no of  chocolates:");
+int n2=sc.nextInt();
+chocolates o1=new chocolates();
+sweets o2=new sweets();
+o1.weight(n1,n2);
+o1.tw();
+o2.weight(n1,n2);
+o2.num();
+}
+}
